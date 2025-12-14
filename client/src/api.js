a2b9eddf9a60f5121aaab4e8e63d80f160e4aba7
NM <nihe25@hotmail.com>
2025-12-13 19:55:45 -0500

    Update API base URL to Render backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,21 @@
 import axios from 'axios';
 
+/*
+  API configuration
+  Uses deployed Render backend
+*/
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://library-manager-api-g7zt.onrender.com/api',
+});
+
+// Attach token if it exists
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
+
