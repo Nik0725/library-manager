738e8b644507658b25642605d48930da5cb4d776
NM <nihe25@hotmail.com>
2025-12-13 20:06:45 -0500

    Fix CORS

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://library-database-manager.netlify.app'
+  ],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
